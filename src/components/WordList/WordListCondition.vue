73ba42bd087f1ea400460f25927b7e9c85a45a78
Kuwana <jkwe.1319@gmail.com>
2024-09-19 21:11:21 +0900

    Merged filter and sort component into one dialog.

--- src/components/WordList/WordListCondition.vue
+++ src/components/WordList/WordListCondition.vue
@@ -1,22 +1,44 @@
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel>
-      <v-expansion-panel-title static>
-        <span class="text-caption">Search & Sort</span>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <WordListFilter @updateFilter="onUpdateFilter"></WordListFilter>
-        <WordSort></WordSort>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <div class="d-flex justify-end">
+    <v-btn size="small" variant="plain" @click="filterDialog = true">
+      <v-icon size="x-large"> mdi-filter-outline </v-icon>
+      <v-dialog v-model="filterDialog" :fullscreen="isFullscreen">
+        <WordListFilter
+          @updateFilter="onUpdateFilter"
+          @close="filterDialog = false"
+        ></WordListFilter>
+      </v-dialog>
+    </v-btn>
+    <!-- <v-btn size="small" variant="plain">
+      <v-icon size="x-large"> mdi-sort-clock-ascending-outline </v-icon>
+      <v-menu activator="parent">
+        <v-list density="compact">
+          <v-list-item>Date Desc</v-list-item>
+          <v-list-item>Date Asc</v-list-item>
+          <v-list-item>Alph Desc</v-list-item>
+          <v-list-item>Alph Asc</v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn> -->
+  </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
+
 import WordListFilter from './WordListFilter.vue';
 import WordSort from './WordSort.vue';
+import { useDisplay } from 'vuetify/lib/framework.mjs';
 
 const emits = defineEmits(['updateFilter', 'updateSort']);
+
+const display = useDisplay();
+
+const filterDialog = ref(false);
+
+const isFullscreen = computed(() => {
+  return display.xs.value;
+});
 
 function onUpdateFilter(value) {
   emits('updateFilter', value);
--- src/components/WordList/WordListFilter.vue
+++ src/components/WordList/WordListFilter.vue
@@ -1,30 +1,52 @@
 <template>
-  <v-container>
-    <v-text-field
-      prepend-icon="mdi-magnify"
-      label="search"
+  <v-card class="mx-auto pa-4 pb-8" elevation="8" max-width="100%" rounded="lg">
+    <div class="pb-3 text-h6 text-medium-emphasis d-flex justify-space-between">
+      <v-icon size="small"> mdi-filter-outline </v-icon>
+      Word Filter
+      <v-icon size="small" @click="emits('close')"> mdi-close </v-icon>
+    </div>
+    <v-divider class="mb-2"></v-divider>
+
+    <div class="text-subtitle-1 text-medium-emphasis">Word</div>
+
+    <v-text-field density="compact" variant="outlined" v-model="inputWord"></v-text-field>
+
+    <div class="text-subtitle-1 text-medium-emphasis">Type</div>
+
+    <v-responsive class="overflow-y-auto" max-height="280">
+      <v-chip-group multiple filter column v-model="selectedTypes">
+        <WordTypeChip
+          v-for="key in typeKeys"
+          :key="key"
+          :type-key="key"
+          :value="key"
+          :variant="chipStyle(key)"
+          size="small"
+        ></WordTypeChip>
+      </v-chip-group>
+    </v-responsive>
+
+    <div class="text-subtitle-1 text-medium-emphasis">Sort</div>
+
+    <v-select
+      :items="sortItemList"
+      item-title="title"
+      item-value="value"
+      v-model="sortItem"
       density="compact"
-      v-model="inputWord"
-    ></v-text-field>
-    <v-chip-group multiple filter column v-model="selectedTypes">
-      <WordTypeChip
-        v-for="key in typeKeys"
-        :key="key"
-        :type-key="key"
-        :value="key"
-        :variant="chipStyle(key)"
-        size="small"
-      ></WordTypeChip>
-    </v-chip-group>
-  </v-container>
+      variant="outlined"
+    ></v-select>
+
+    <v-btn color="blue" size="large" variant="tonal" block @click="apply"> Apply </v-btn>
+  </v-card>
 </template>
 
 <script setup>
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { useWordStore } from '@/stores/word';
 import WordTypeChip from '../parts/WordTypeChip.vue';
 
-const emits = defineEmits(['updateFilter']);
+const emits = defineEmits(['updateFilter', 'close']);
 
 const store = useWordStore();
 const typeKeys = store.getTypeKeys();
@@ -39,16 +61,33 @@ const filterCondition = computed(() => {
   return condition;
 });
 
-watch(selectedTypes, () => {
-  emits('updateFilter', filterCondition.value);
-});
-
-watch(inputWord, () => {
-  emits('updateFilter', filterCondition.value);
-});
+const sortItemList = [
+  {
+    title: 'Date Asc',
+    value: 1
+  },
+  {
+    title: 'Date Desc',
+    value: 2
+  },
+  {
+    title: 'Alph - Asc',
+    value: 3
+  },
+  {
+    title: 'Alph - Desc',
+    value: 4
+  }
+];
+const sortItem = ref(sortItemList[0]);
 
 function chipStyle(key) {
   const isSelected = selectedTypes.value.includes(key);
   return isSelected ? 'elevated' : 'outlined';
+}
+
+function apply() {
+  emits('updateFilter', filterCondition.value);
+  emits('close');
 }
 </script>
