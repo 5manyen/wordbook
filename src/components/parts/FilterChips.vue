f72f99436e4e1156995d5dea4f361de826e50fa6
Kuwana <jkwe.1319@gmail.com>
2024-09-24 20:18:42 +0900

    Created a component to show active filter condition.

---
+++ src/components/parts/FilterChips.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-responsive class="overflow-y-auto" max-height="280">
+    <v-chip v-for="key in keys" closable @click:close="onClose(key)">
+      {{ key }}
+    </v-chip>
+  </v-responsive>
+</template>
+
+<script setup>
+const props = defineProps({
+  keys: Array
+});
+
+const emits = defineEmits('clearFilter');
+
+function onClose(key) {
+  emits('clearFilter', key);
+}
+</script>
